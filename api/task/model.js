2c89e101753131ac52ae1aecbcdbd4a85035bf34
Andrew Castillo <andrewcast32@gmail.com>
2021-05-30 14:41:56 -0700

    task get request

--- api/task/model.js
+++ api/task/model.js
@@ -1,10 +1,12 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig');
 
-const get = () => {
-  return db('tasks')
+const getTasks = () => {
+  return db('tasks as t')
+    .leftJoin('projects as p','p.project_id','t.task_id')
+    .select('t.task_id','t.task_description', 't.task_notes','t.task_completed')
 }
 
 module.exports = {
-  get
+  getTasks
 }
--- api/task/router.js
+++ api/task/router.js
@@ -5,16 +5,24 @@ const Tasks = require('./model');
 const tasRouter = express.Router();
 
 tasRouter.get('/tasks', (req, res, next) => {
-  Tasks.get()
-    .then(tasks => {
-      if(tasks){
-        res.status(200).json(tasks)
+  Tasks.getTasks()
+    .then(task => {
+      if(task){
+        res.status(200).json(task)
       }else{
         res.status(400).json({message: 'Project request fail'})
       }
     })
     .catch(next)
 })
+
+// tasRouter.post('/tasks', (req, res, next) => {
+//   Tasks.addProject(req.body)
+//     .then(task => {
+//       res.status(201).json(task)
+//     })
+//     .catch(next)
+// })
 
 
 module.exports = tasRouter;
